f13e82272c1a5ede288a87184a8da570d40f210b
ckie <git-525ff67@ckie.dev>
2022-02-27 22:52:36 +0200

    make da cursor blink

--- config.def.h
+++ config.def.h
@@ -162,7 +162,8 @@ Glyph style[] = {{' ',ATTR_ITALIC|ATTR_FAINT,15,16}, {' ',ATTR_ITALIC,232,11},
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorstyle = 1;
+static Rune stcursor = 0x2603; /* snowman (U+2603) */
 
 /*
  * Default columns and rows numbers
